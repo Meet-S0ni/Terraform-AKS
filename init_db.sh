5769eeb9dc672871bbd0811a20e877cc2dbd99d1
Meet <89190035+Meet-S0ni@users.noreply.github.com>
2023-11-01 23:28:54 +0530

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -2,3 +2,5 @@ kubectl exec -it postgres -n db -- bash -c 'psql -U postgres -c "CREATE DATABASE
 kubectl exec -it postgres -n db -- bash -c 'psql -U postgres -d wordpress -c "CREATE USER wordpress WITH PASSWORD '\''postgrespassword'\'';"'
 kubectl exec -it postgres -n db -- bash -c 'psql -U postgres -d wordpress -c "CREATE SCHEMA IF NOT EXISTS wordpress AUTHORIZATION wordpress;"'
 kubectl exec -it postgres -n db -- bash -c 'psql -U postgres -d wordpress -c "ALTER USER wordpress SET search_path = wordpress;"'
+
+test
